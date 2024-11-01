a29dc2576bc1059e01130738f1edfd1c66771b07
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-11-01 10:08:03 +0000

    db connection 1

---
+++ dbconnection.vb
@@ -0,0 +1,22 @@
+﻿Imports MySql.Data.MySqlClient
+Module dbconnection
+    Public conn As New MySqlConnection
+    Dim result As Boolean
+    Public cmd As New MySqlCommand
+    Public da As New DataTable
+    Public dr As MySqlDataReader
+    Public i As Integer
+
+    Public Function dbconn() As Boolean
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.ConnectionString = "server=localhost;username=root;password=;port=3306;database=dp_pos"
+                result = True
+            End If
+        Catch ex As Exception
+            result = False
+            MsgBox("Server not connected!", vbExclamation)
+        End Try
+        Return result
+    End Function
+End Module
